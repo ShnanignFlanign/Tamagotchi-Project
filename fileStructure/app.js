4383edc4842e74407ffa438f1a1efee4c86a4af2
ShnanignFlanign <seanpflan@live.com>
2022-07-06 16:59:24 -0700

    some functionality

--- fileStructure/app.js
+++ fileStructure/app.js
@@ -7,6 +7,8 @@ class Tamagotchi {
         this.energy = 3;
         this.hunger = 3;
         this.excitement = 3;
+        this.intellect = 0;
+        this.strength = 0;
     }
     plusAge (someNum) {
         this.age += someNum;
@@ -29,6 +31,41 @@ class Tamagotchi {
     minusExcitement (someNum) {
         this.excitement -= someNum;
     }
+    plusIntellect (someNum) {
+        this.intellect += someNum;
+    }
+    minusIntellect (someNum) {
+        this.intellect -= someNum;
+    }
+    plusStrength (someNum) {
+        this.strength += someNum;
+    }
+    minusStrength (someNum) {
+        this.strength -= someNum;
+    }
+}
+
+const playWithPet = (pet) => {
+    console.log("we're playing")
+    pet.plusExcitement(1)
+    pet.plusHunger(1)
+    pet.minusEnergy(1)
+}
+
+const readToPet = (pet) => {
+    pet.plusIntellect(1)
+    pet.minusExcitement(1)
+}
+
+const feedPet = (pet) => {
+    pet.minusHunger(1)
+    pet.plusEnergy(0.5)
+}
+
+const exercisePet = (pet) => {
+    pet.plusStrength(1)
+    pet.plusHunger(2)
+    pet.minusEnergy(2)
 }
 
 //death/loss conditions:
@@ -37,16 +74,22 @@ class Tamagotchi {
 //   hunger goes to 10
 
 //interactions list: 
+
 // toggle lights (lights off let tamagotchi sleep, increases energy)
-// feed pet (decreases hunger)
-// play w/ pet (increases excitement)
+// feed pet (decreases hunger, marginally increases energy)
+// play w/ pet (increases excitement, decreases energy, increases hunger)
+
+// optional/potential interactions:
+// read to pet (increases intellect, decreases excitement)
+// exercise (increases strength, increases hunger, decreases energy)
 
 //static things:
 //hunger increases over time
 //excitement decreases over time
 //energy decreases over time (toggling lights off reverses effects)
 // age increases over time (at a slower rate than every other status)
+//    tamagotchi evolves at age 5 and 10
 
 //general notes for conditions:
 // sleep state/animation when lights off
-// restrict ability to feed/play with pet when sleep state active
+// restrict ability to feed/play with/read to pet when sleep state active
